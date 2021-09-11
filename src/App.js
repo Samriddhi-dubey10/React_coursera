bab7966dc2fc8a1ebb2aaa503501c565a61981dd
Samriddhi-dubey10 <dubeydinu1974@gmail.com>
2021-09-11 18:38:26 +0530

    state and props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+import { Component } from 'react';
 
-function App() {
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      dishes: DISHES
+    };
+  }
+  render(){
   return (
     <div>
       <Navbar dark color="primary">
@@ -11,9 +21,10 @@ function App() {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
     </div>
   );
+  }
 }
 
 export default App;
